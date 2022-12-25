36e1b1ea0628cc0dcb3a64c53e34be3bbf5d609a
Kaede Fujisaki <psi@7io.org>
2022-12-25 17:47:06 +0900

    おまじない

--- fujitsu-mpi-sys/gen.sh
+++ fujitsu-mpi-sys/gen.sh
@@ -4,4 +4,6 @@ source "/vol0004/apps/oss/spack/share/spack/setup-env.sh"
 spack load /pvhwhce # load LLVM 12
 
 set -eux
+# TO INSTALL:
+#   cargo install bindgen-cli
 bindgen /opt/FJSVxtclanga/tcsds-1.2.36/include/mpi/fujitsu/mpi.h -o src/ffi.rs
