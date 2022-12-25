04ea9aec1f19bc69708d11ad059acf1ede4a9026
Kaede Fujisaki <psi@7io.org>
2022-12-25 15:25:26 +0900

    renamed to ffi

--- fujitsu-mpi-sys/gen.sh
+++ fujitsu-mpi-sys/gen.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 source "/vol0004/apps/oss/spack/share/spack/setup-env.sh"
 spack load /pvhwhce # load LLVM 12
-bindgen /opt/FJSVxtclanga/tcsds-1.2.36/include/mpi/fujitsu/mpi.h -o src/mpi.rs
+bindgen /opt/FJSVxtclanga/tcsds-1.2.36/include/mpi/fujitsu/mpi.h -o src/ffi.rs
