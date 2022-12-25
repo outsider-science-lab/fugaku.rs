655d6314bd69d20b9de45c4f1efb76d379abe515
Kaede Fujisaki <psi@7io.org>
2022-12-25 15:17:57 +0900

    count cores

--- bed/src/main.rs
+++ bed/src/main.rs
@@ -3,6 +3,6 @@ fn main() -> anyhow::Result<()> {
   let universe = fujitsu_mpi::initialize()?;
   println!("Initialized");
   let world = universe.world();
-  println!("Size: {}, rank: {}", world.size()?, world.rank()?);
+  println!("Size: {}, rank: {}, cores={}", world.size()?, world.rank()?, num_cpus::get());
   Ok(())
 }
