ce276d7fdfa40dadc119a4cb60e58a917b7e9be8
Kaede Fujisaki <psi@7io.org>
2022-12-30 18:16:15 +0900

    we don't need this line...

--- job.sh
+++ job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PJM -L "node=16"
+#PJM -L "node=2"
 #PJM -L "rscgrp=small"
 #PJM -L "elapse=10:00"
 #PJM -g hp220271
@@ -14,8 +14,6 @@
 
 # See https://www.fugaku.r-ccs.riken.jp/doc_root/en/user_guides/use_latest/LyeredStorageAndLLIO/TheSecondLayerStrage.html#common-file-distribution-function-llio-transfer
 #/usr/bin/llio_transfer <path_to_file>
-
-cd "$(cd "$(dirname "$(readlink -f "$(git rev-parse --show-toplevel)")")" && pwd)"
 
 source /vol0004/apps/oss/llvm-v14.0.1/init.sh
 
