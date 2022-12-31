02d8fce26f3b760e91b09f7b3770445424a63225
Kaede Fujisaki <psi@7io.org>
2022-12-31 13:53:39 +0900

    add gather

---
+++ examples/gather.rs
@@ -0,0 +1,27 @@
+fn main() -> anyhow::Result<()> {
+  let mut universe = fujitsu_mpi::initialize()?;
+  println!("Initialized");
+  let mut world = universe.world();
+  let size = world.size()?;
+  let rank = world.rank()?;
+  let root: usize = 0;
+  println!("size = {}, rank = {}", size, rank);
+  let mut send_buff: Vec<u64> = if rank == 0 {
+    vec![1, 2, 3]
+  } else {
+    vec![4, 5, 6]
+  };
+  let mut recv_buff: Vec<u64> = if rank == root {
+    vec![0; 6]
+  } else {
+    vec![]
+  };
+  println!("SendBuf: {:?} / RecvBuf {:?}", send_buff, recv_buff);
+  world.gather(
+    &mut send_buff,
+    &mut recv_buff,
+    0,
+  )?;
+  println!("SendBuf: {:?} / RecvBuf {:?}", send_buff, recv_buff);
+  Ok(())
+}
