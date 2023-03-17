abb419fda175fb6ba447833eda3993e71a1e9d6a
Kaede Fujisaki <psi@7io.org>
2023-03-18 00:18:12 +0900

    nullptr

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,10 @@
 
 pub fn malloc<T>() -> T {
-  return unsafe {
+  unsafe {
     std::mem::MaybeUninit::<T>::zeroed().assume_init()
-  };
+  }
+}
+
+pub fn nullptr<T>() -> *mut T {
+  std::ptr::null_mut()
 }
