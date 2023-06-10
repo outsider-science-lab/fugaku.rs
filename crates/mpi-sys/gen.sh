b7c4f1b8d119c54cee2f69be4102b1230f4bafa2
Kaede Fujisaki <psi@7io.org>
2023-06-11 00:15:11 +0900

    Update gen.sh

--- crates/mpi-sys/gen.sh
+++ crates/mpi-sys/gen.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
-cd "$(cd "$(dirname "$(readlink -f "$0")")" && pwd)"
-source "/vol0004/apps/oss/spack/share/spack/setup-env.sh"
-spack load /pvhwhce # load LLVM 12
 
-set -eux
 # TO INSTALL:
 #   cargo install bindgen-cli
+
+cd "$(cd "$(dirname "$(readlink -f "$0")")" && pwd)"
+source "/vol0004/apps/oss/spack/share/spack/setup-env.sh"
+
+spack load /cysddka || false
+
+set -eux
 
 compiler_dir="$(cd "$(dirname "$(which mpifccpx)")" && cd .. && pwd)"
 
