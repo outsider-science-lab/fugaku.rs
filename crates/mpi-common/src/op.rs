1d8b89b3f99062b108744831fedabe50d273a112
Kaede Fujisaki <psi@7io.org>
2024-10-08 19:49:12 +0900

    2 op

--- crates/mpi-common/src/op.rs
+++ crates/mpi-common/src/op.rs
@@ -16,6 +16,21 @@ pub enum Op {
   MinLocation,
   Sum,
   Prod,
+  /**
+   * https://docs.open-mpi.org/en/v5.0.x/man-openmpi/man3/MPI_Rget_accumulate.3.html#mpi-rget-accumulate
+   * A new predefined operation, MPI_REPLACE, is defined.
+   * It corresponds to the associative function f(a, b) =b; that is,
+   * the current value in the target memory is replaced by the value supplied by the origin.
+   */
+  Replace,
+  /**
+   * https://docs.open-mpi.org/en/v5.0.x/man-openmpi/man3/MPI_Rget_accumulate.3.html#mpi-rget-accumulate
+   * A new predefined operation, MPI_NO_OP, is defined.
+   * It corresponds to the assiciative function f(a, b) = a;
+   * that is the current value in the target memoryis returned in the result buffer at the origin and
+   * no operation is performed on the target buffer.
+   */
+  NoOp,
 }
 
 impl Op {
@@ -35,6 +50,8 @@ impl Op {
         &Self::MinLocation => addr_of_mut!(ffi::ompi_mpi_op_minloc),
         &Self::Sum => addr_of_mut!(ffi::ompi_mpi_op_sum),
         &Self::Prod => addr_of_mut!(ffi::ompi_mpi_op_prod),
+        &Self::Replace => addr_of_mut!(ffi::ompi_mpi_op_replace),
+        &Self::NoOp => addr_of_mut!(ffi::ompi_mpi_op_no_op),
       }
     };
     op as ffi::MPI_Op
