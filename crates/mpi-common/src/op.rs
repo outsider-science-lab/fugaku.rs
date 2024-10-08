da80b9529851b927196cebb2838981eeca9d3231
Kaede Fujisaki <psi@7io.org>
2024-10-08 16:53:31 +0900

    Add Predefined operator

--- crates/mpi-common/src/op.rs
+++ crates/mpi-common/src/op.rs
@@ -4,7 +4,18 @@ use mpi_sys as ffi;
 
 pub enum Op {
   Null,
+  BitAnd,
+  BitOr,
+  BitXor,
+  LogicalAnd,
+  LogicalOr,
+  LogicalXor,
+  Max,
+  MaxLocation,
+  Min,
+  MinLocation,
   Sum,
+  Prod,
 }
 
 impl Op {
@@ -12,7 +23,18 @@ impl Op {
     let op = unsafe {
       match self {
         &Self::Null => addr_of_mut!(ffi::ompi_mpi_op_null),
+        &Self::BitAnd => addr_of_mut!(ffi::ompi_mpi_op_band),
+        &Self::BitOr => addr_of_mut!(ffi::ompi_mpi_op_bor),
+        &Self::BitXor => addr_of_mut!(ffi::ompi_mpi_op_bxor),
+        &Self::LogicalAnd => addr_of_mut!(ffi::ompi_mpi_op_land),
+        &Self::LogicalOr => addr_of_mut!(ffi::ompi_mpi_op_lor),
+        &Self::LogicalXor => addr_of_mut!(ffi::ompi_mpi_op_lxor),
+        &Self::Max => addr_of_mut!(ffi::ompi_mpi_op_max),
+        &Self::MaxLocation => addr_of_mut!(ffi::ompi_mpi_op_maxloc),
+        &Self::Min => addr_of_mut!(ffi::ompi_mpi_op_min),
+        &Self::MinLocation => addr_of_mut!(ffi::ompi_mpi_op_minloc),
         &Self::Sum => addr_of_mut!(ffi::ompi_mpi_op_sum),
+        &Self::Prod => addr_of_mut!(ffi::ompi_mpi_op_prod),
       }
     };
     op as ffi::MPI_Op
