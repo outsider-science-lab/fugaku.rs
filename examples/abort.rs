b93e688ffca8cab0c321c168c1020e80975dd33b
Kaede Fujisaki <psi@7io.org>
2024-10-22 10:02:42 +0900

    Fix examples

--- examples/abort.rs
+++ examples/abort.rs
@@ -2,7 +2,7 @@ fn main() -> anyhow::Result<()> {
   let request = mpi::ThreadLevel::Multiple;
   let mut universe = mpi::initialize_thread(request)?;
   println!("Initialized");
-  let world = universe.world();
+  let mut world = universe.world();
   let size = world.size()?;
   let rank = world.rank()?;
   println!("size = {}, rank = {}", size, rank);
--- examples/hello-mpi.rs
+++ examples/hello-mpi.rs
@@ -1,7 +1,7 @@
 fn main() -> anyhow::Result<()> {
   let mut universe = mpi::initialize()?;
   println!("Initialized");
-  let world = universe.world();
+  let mut world = universe.world();
   println!("size = {}, rank = {}", world.size()?, world.rank()?);
   Ok(())
 }
